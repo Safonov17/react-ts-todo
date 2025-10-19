862efd1a5e1e305cafa8cb437d7bf13d43d3e086
Safonov17 <yagitarist17@gmail.com>
2025-10-20 00:54:01 +0300

    #2

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import {ToDoList} from './pages/ToDoList';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/scss/index.scss'
+import './assets/scss/normalize.scss'
+import { ToDoListPage } from './pages/ToDoListPage'
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <ToDoList />
-  </React.StrictMode>
-);
-
+	<React.StrictMode>
+		<ToDoListPage />
+	</React.StrictMode>
+)
